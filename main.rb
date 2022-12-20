8768b1d63358f867a83d6f837697e6b7b70c7bfc
mdtthanh <maithanh0131@gmail.com>
2022-12-20 09:47:01 +0000

    first commit

--- main.rb
+++ main.rb
@@ -1,1 +1,8 @@
-puts "Hello, World!"
+puts 'Hello, World!'
+puts 'Hello, World!'
+puts 'Hello, World!'
+puts 'Hello, World!'
+puts 'Hello, World!'
+puts 'Hello, World!'
+puts 'Hello, World!'
+puts 'Hello, World!'
